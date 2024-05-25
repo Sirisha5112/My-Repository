68df7ab5ae68e582dde7eb72aa282512215e041a
Sirisha5112 <agoorsirisha5112@gmail.com>
2024-05-25 17:57:57 +0530

    my first code

---
+++ Example1.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+public class Example1 {
+	
+	public static void main(String[] args) 
+	{
+		int first=100;
+		int second=500,third=300;
+		double fourth=350.78;
+		int add=first+second;
+		int sub=second-first;
+		int mul=second*third;
+		double div=third/fourth;
+		System.out.println("Welcome to edubrige java full stack course");
+		System.out.println("ADDITION :"+add);
+		System.out.println("SUBTRACTION:"+sub);
+		System.out.println("MULTIPLICATION:"+mul);
+		System.out.println("DIVISION:"+div);
+		System.out.println("MADULO DIVISION:"+(first%second));
+	}
+
+}
